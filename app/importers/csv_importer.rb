762aae6edf1e9b51ea5fac3ccd70e8c438e5cc60
Iris <ayame@mailbox.org>
2018-11-08 10:44:32 +0100

    Issue #15 working forc csv files with empty lines

--- app/importers/csv_importer.rb
+++ app/importers/csv_importer.rb
@@ -6,8 +6,21 @@ class CsvImporter
   end
   def import
     CSV.foreach(@file) do |row|
+      next if row.empty?
       image = Image.new
       image.title = [row[1]]
+      image.source = [row[2]]
+      image.visibility = "open"
+      image.depositor = @user.email
+      # Attach the image file and run it through the actor stack
+      # Try entering Hyrax::CurationConcern.actor on a console to see all of the
+      # actors this object will run through.
+      image_binary = File.open("#{::Rails.root}/spec/fixtures/images/#{row[0]}")
+      uploaded_file = Hyrax::UploadedFile.create(user: @user, file: image_binary)
+      attributes_for_actor = { uploaded_files: [uploaded_file.id] }
+      env = Hyrax::Actors::Environment.new(image, ::Ability.new(@user), attributes_for_actor)
+      Hyrax::CurationConcern.actor.create(env)
+      image_binary.close
     end
   end
 end
--- spec/importers/csv_importer_spec.rb
+++ spec/importers/csv_importer_spec.rb
@@ -9,12 +9,28 @@ RSpec.describe CsvImporter do
     DatabaseCleaner.clean
     ActiveFedora::Cleaner.clean!
   end
-
+  it "imports a csv" do
+    expect { CsvImporter.new(three_line_example).import }.to change { Image.count }.by 3
+  end
   it "puts the title into the title field" do
     CsvImporter.new(one_line_example).import
     imported_image = Image.first
     expect(imported_image.title.first).to eq 'A Cute Dog'
   end
-
-
+  it "puts the url into the source field" do
+    CsvImporter.new(one_line_example).import
+    imported_image = Image.first
+    expect(imported_image.source.first).to eq 'https://www.pexels.com/photo/animal-blur-canine-close-up-551628/'
+  end
+  it "creates publicly visible objects" do
+    CsvImporter.new(one_line_example).import
+    imported_image = Image.first
+    expect(imported_image.visibility).to eq 'open'
+  end
+  it "attaches a file" do
+    CsvImporter.new(one_line_example).import
+    imported_image = Image.first
+    expect(imported_image.members.first).to be_instance_of(FileSet)
+    expect(imported_image.members.first.title.first).to eq 'dog.jpg'
+  end
 end
