b382169ab5b997615630517585e170e59bfac477
Iris <ayame@mailbox.org>
2018-11-08 14:54:53 +0100

    Issue #11 use title row for matching fields"

--- app/importers/csv_importer.rb
+++ app/importers/csv_importer.rb
@@ -5,17 +5,21 @@ class CsvImporter
     @user = ::User.batch_user
   end
   def import
-    CSV.foreach(@file) do |row|
+    parsed_csv =  CSV.parse(
+      File.read(@file),
+      headers: true
+    )
+    parsed_csv.each do |row|
       next if row.empty?
       image = Image.new
-      image.title = [row[1]]
-      image.source = [row[2]]
+      image.title = [row['title']]
+      image.source = [row['source']]
       image.visibility = "open"
       image.depositor = @user.email
       # Attach the image file and run it through the actor stack
       # Try entering Hyrax::CurationConcern.actor on a console to see all of the
       # actors this object will run through.
-      image_binary = File.open("#{::Rails.root}/spec/fixtures/images/#{row[0]}")
+      image_binary = File.open("#{::Rails.root}/spec/fixtures/images/#{row['filename']}")
       uploaded_file = Hyrax::UploadedFile.create(user: @user, file: image_binary)
       attributes_for_actor = { uploaded_files: [uploaded_file.id] }
       env = Hyrax::Actors::Environment.new(image, ::Ability.new(@user), attributes_for_actor)
