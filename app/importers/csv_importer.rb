4e97396cb352e98056b3969bb0faad92fc08aaf5
joe lakes <jlakes@tcd.ie>
2018-11-08 01:34:14 -0800

    Issue #10 Import CSV file

--- app/importers/csv_importer.rb
+++ app/importers/csv_importer.rb
@@ -1,3 +1,18 @@
-class CsvImporter < ActiveFedora::Base
+require 'csv'
+ class CsvImporter
+  def initialize(file)
+    @file = file
+    #@user = ::User.batch_user
+  end
+
+  def import
+    CSV.foreach(@file) do |row|
+      byebug
+      image = Image.new
+      image.title = ["something"]
+      image.depositor = "something"
+      image.save
+    end
+  end
 
 end
--- spec/importers/csv_importer_spec.rb
+++ spec/importers/csv_importer_spec.rb
@@ -1,5 +1,20 @@
+# frozen_string_literal: true
 require 'rails_helper'
+require 'active_fedora/cleaner'
 
-RSpec.feature 'Instantiate the class' do
-  CsvImporter.create()
+
+RSpec.describe CsvImporter do
+
+  let(:one_line_example)       { 'spec/fixtures/one_line_example.csv' }
+  let(:three_line_example)     { 'spec/fixtures/three_line_example.csv' }
+
+  before do
+    DatabaseCleaner.clean
+    ActiveFedora::Cleaner.clean!
+  end
+
+  it "imports a csv" do
+    expect { CsvImporter.new(three_line_example).import }.to change { Image.count }.by 3
+  end
+
 end
